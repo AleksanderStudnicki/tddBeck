38835b98a6af026d4f4fabd8a69fc5f1ccf61d96
AleksanderStudnicki <alek.studnicki@gmail.com>
2019-11-19 01:27:14 +0100

    passed 5th chapter

---
+++ src/test/java/FrancTest.java
@@ -0,0 +1,21 @@
+import com.studnicki.tddBeck.Franc;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FrancTest {
+    @Test
+    public void testMultiplication(){
+        Franc five = new Franc(5);
+        assertTrue(new Franc(10).equals(five.times(2)));
+        assertTrue(new Franc(15).equals(five.times(3)));
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+    }
+
+}
